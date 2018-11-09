aaf22f6f7585ed115a7566e2cc8daed08df1d72f
EstebanBV01 <Johan@192.168.1.3>
2018-11-09 17:24:39 -0600

    .

--- TareaProgramada/src/JuegoDeLaVida/InteraccionUs/ManejoInterfazGraf.java
+++ TareaProgramada/src/JuegoDeLaVida/InteraccionUs/ManejoInterfazGraf.java
@@ -12,19 +12,22 @@ import javax.swing.JOptionPane;
  * @author usuario
  */
 public class ManejoInterfazGraf {
-    
-        ImageIcon icon = new ImageIcon("logo1.png");
         Reglas reglasGame = new Reglas();
         LogicaDeNavegacion logic = new LogicaDeNavegacion();
-        
+       // Cuadricula[][] cuad= new Cuadricula[][]{};
         public void startGame () {
             int cant = Integer.parseInt(JOptionPane.showInputDialog(null, "Tamaño Juego"));
             Cuadricula cuadri = new Cuadricula(cant);
+            reglasGame.fillMatrix(cuadri.getMatriz());
             int celdasAle = Integer.parseInt(JOptionPane.showInputDialog(null, "Espacios Aleatorios"));
             cuadri.random(celdasAle);
+            cuadri.setPositions(cuadri.getMatriz());
             logic.stringMatrix(cuadri.getMatriz());
-            reglasGame.Rules(cuadri.getMatriz());
-            logic.stringMatrix(cuadri.getMatriz());
+            //reglasGame.Rules(cuadri.getMatriz());
+            reglasGame.encuentraVivos(cuadri.getMatriz());
+            
+            
+            
         }
 
         
