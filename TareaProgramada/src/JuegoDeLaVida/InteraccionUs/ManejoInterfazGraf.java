307e650fb5861d533789b8e090b5436d5e9754f6
Juan Carlos <Satellaizar@192.168.1.104>
2018-11-09 18:23:01 -0600

    Laboratorio3, Universidad De Costa Rica, Recinto Tacares

--- TareaProgramada/src/JuegoDeLaVida/InteraccionUs/ManejoInterfazGraf.java
+++ TareaProgramada/src/JuegoDeLaVida/InteraccionUs/ManejoInterfazGraf.java
@@ -14,21 +14,24 @@ import javax.swing.JOptionPane;
 public class ManejoInterfazGraf {
         Reglas reglasGame = new Reglas();
         LogicaDeNavegacion logic = new LogicaDeNavegacion();
-       // Cuadricula[][] cuad= new Cuadricula[][]{};
+        boolean decision=true;
         public void startGame () {
-            boolean decision=true;
+            
+            
             int cant = Integer.parseInt(JOptionPane.showInputDialog(null, "Tamaño Juego"));
             Cuadricula cuadri = new Cuadricula(cant);
             reglasGame.fillMatrix(cuadri.getMatriz());
             int celdasAle = Integer.parseInt(JOptionPane.showInputDialog(null, "Espacios Aleatorios"));
             cuadri.random(celdasAle);
-            int fila=Integer.parseInt(JOptionPane.showInputDialog("Digite La Fila Que Desea"));
-            int col=Integer.parseInt(JOptionPane.showInputDialog("Digite La Columna Que Desea"));
+
             do {
+                 int fila=Integer.parseInt(JOptionPane.showInputDialog("Digite La Fila Que Desea"));
+            int col=Integer.parseInt(JOptionPane.showInputDialog("Digite La Columna Que Desea"));
                 cuadri.setPositions(fila, col);
                 logic.stringMatrix(cuadri.getMatriz());
                 decision = Boolean.parseBoolean(JOptionPane.showInputDialog("Desea añadir otra? \n[true] Para Si! \n[false] Para No!"));
-                }  while (decision == true); 
+                
+            } while (decision == true); 
                 reglasGame.Rules(cuadri.getMatriz());
                 reglasGame.encuentraVivos(cuadri.getMatriz());
             
