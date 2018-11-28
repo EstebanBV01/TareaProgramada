b5cd8efa089507dd66a31862d5041ca658375a23
EstebanBV01 <Johan@192.168.1.3>
2018-11-27 21:41:26 -0600

    prueba en el main comentario

--- TareaProgramada/Programada2/src/Game/Main.java
+++ TareaProgramada/Programada2/src/Game/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //prueba de push 27-11-18 9:40pm
     }
     
 }
